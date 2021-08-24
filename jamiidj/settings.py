4e447c8c82dab6c0361e9773bd8a5b499b093a6b
KingMabuza <amal@amallevi.com>
2021-08-24 16:04:38 +0200

    Changed URL for logo

--- jamiidj/settings.py
+++ jamiidj/settings.py
@@ -124,11 +124,6 @@ USE_TZ = True
 STATIC_ROOT = Path(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    '/static'
-]
-
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR, 'media')
 
